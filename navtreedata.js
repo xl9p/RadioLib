dea44a7ac402cfd3fcbd4ee03eb6c242df89403a
xl9p <31505793+xl9p@users.noreply.github.com>
2025-03-15 11:37:06 +0000

    Deploying to gh-pages from  @ f0e686eaa959f1e0b796fd10cb051d4ba2691b9f 🚀

---
+++ navtreedata.js
@@ -0,0 +1,67 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "RadioLib", "index.html", [
+    [ "RadioLib Documentation", "index.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_a_f_s_k_8h_source.html",
+"class_l_r1120.html#a4c6b867146f00de5c45aeb4164d51cc5",
+"class_physical_layer.html#a923654706eff5118ef6e84214e837f27",
+"class_s_t_m32_w_lx.html#ad96d18fea2a7fffa959b64c8c5d993a8",
+"class_s_x127x.html#a48ca43e6aad02815fa1507f0f0831c54",
+"classn_r_f24.html#a5957f06a891d3d9c07e87b59c239ce56",
+"lr1110__transceiver__0306_8h.html#a9d71ed93e8016b97a4d70b4194f622db"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/classes_b.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['pagerclient_0',['PagerClient',['../class_pager_client.html',1,'']]],
+  ['physicallayer_1',['PhysicalLayer',['../class_physical_layer.html',1,'']]],
+  ['picohal_2',['PicoHal',['../class_pico_hal.html',1,'']]],
+  ['pihal_3',['PiHal',['../class_pi_hal.html',1,'']]]
+];
